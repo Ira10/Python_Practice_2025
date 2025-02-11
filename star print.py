6136c01309fa34bf362030d0e107f2626124b54b
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2025-02-11 15:39:18 +0530

    Add files via upload

---
+++ star print.py
@@ -0,0 +1,15 @@
+# for i in range(5): ## line
+#     print('*')
+    # for j in range(i):
+    #     print('*')
+
+# *
+# **
+# ***
+# ****
+# *****
+
+for i in range(5):
+    print('* '*(i+1))
+
+print("Annu "*23)
