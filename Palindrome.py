d0d8cfd8ca65b03c63cb25a8a877d6ffd4fe5a54
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2025-02-17 13:12:05 +0530

    Add files via upload

--- Palindrome.py
+++ Palindrome.py
@@ -10,3 +10,14 @@ print(L[::1])  # 545678w
 
 print(L[::-1])  # w876545
 
+num = 12345678910
+print(num[::-1])  # File "/Users/indrani.sarkar/Desktop/PYTHON/Functional/Palindrome.py", line 14, in <module>
+#     print(num[::-1])
+#           ~~~^^^^^^
+# TypeError: 'int' object is not subscriptable
+
+print((str(num))[::-1])  # 01987654321
+
+
+
+
