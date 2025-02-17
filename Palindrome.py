0ae9ba1fe8f8b5e7272957596c6852097a6e8dd5
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2025-02-17 13:08:41 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,12 @@
+### 17th Feb
+
+## https://www.w3resource.com/python-exercises/python-functions-exercise-12.php
+
+### remembered the solution
+
+L = '545678w'
+print(L[1::])  # 45678w
+print(L[::1])  # 545678w
+
+print(L[::-1])  # w876545
+
