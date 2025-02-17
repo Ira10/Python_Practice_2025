13a94b584d75a0129419e49a752a3d6284e37747
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2025-02-17 12:23:48 +0530

    Add files via upload

---
+++ rough_codechef.py
@@ -0,0 +1,20 @@
+# print(int('3.14'))  # ValueError: invalid literal for int() with base 10: '3.14'
+
+# print('2' + '2')  ## 22
+
+# a = 'nhkg'
+# print(a[2])  ## k
+
+# a[2] = '789'
+# print(a)  #    a[2] = '789'
+#     # ~^^^
+# # TypeError: 'str' object does not support item assignment
+
+print(5 // 2) # 2
+print(5%2)   # 1
+print(23// 5) # 4
+print(23 % 5) # 4
+
+
+
+
